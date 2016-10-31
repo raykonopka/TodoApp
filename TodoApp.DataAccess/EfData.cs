0810440004f5b58ed2611b56189f5162c62f2673
raykonopka <konopka.ray@gmail.com>
2016-10-31 10:35:35 -0400

    add data access for todo app

---
+++ TodoApp.DataAccess/EfData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.DataAccess
+{
+    public class EfData
+    {
+        private TodoDBEntities db = new TodoDBEntities();
+
+        //Add an item
+        public bool InsertItem(TodoItem item)
+        {
+            db.TodoItems.Add(item);
+            return db.SaveChanges() > 0;
+        }
+
+        //Delete an item
+        public bool RemoveItem(int ItemId)
+        {
+            List<TodoItem> allItems = GetTodoItems();
+            var matchingItems = allItems.Where(i => i.Id.Equals(ItemId));
+
+            db.TodoItems.Remove(matchingItems.First());
+            return db.SaveChanges() > 0;
+        }
+
+        //Update an item
+        public bool UpdateItem(TodoItem item, EntityState state)
+        {
+            var entry = db.Entry<TodoItem>(item);
+
+            entry.State = state;
+            return db.SaveChanges() > 0;
+        }
+
+
+        //Make item as complete or incomplete
+        public bool UpdateCompletionStatus(TodoItem item, bool completed)
+        {
+            if (completed)
+            {
+                item.Completed = true;
+            }
+            else
+            {
+                item.Completed = false;
+            }
+
+            return db.SaveChanges() > 0;
+        }
+
+        //Get a list of items
+        public List<TodoItem> GetTodoItems()
+        {
+            return db.TodoItems.ToList();
+        }
+
+        //Get a list of compelted items
+        public List<TodoItem> GetTodoItemsComplete()
+        {
+            List<TodoItem> AllItems = GetTodoItems();
+            List<TodoItem> CompletedItems = new List<TodoItem>();
+
+            foreach (TodoItem item in AllItems)
+            {
+                if (item.Completed == true)
+                {
+                    CompletedItems.Add(item);
+                }
+            }
+
+            return CompletedItems;
+        }
+
+
+        //Get a list of incompelte items
+        public List<TodoItem> GetTodoItemsIncomplete()
+        {
+            List<TodoItem> AllItems = GetTodoItems();
+            List<TodoItem> IncompleteItems = new List<TodoItem>();
+
+            foreach (TodoItem item in AllItems)
+            {
+                if (item.Completed != true)
+                {
+                    IncompleteItems.Add(item);
+                }
+            }
+
+            return IncompleteItems;
+        }
+
+    }
+}
---
+++ TodoApp.Tests/EfDataTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.DataAccess;
+using Xunit;
+
+namespace TodoApp.Tests
+{
+    public class EfDataTests
+    {
+        [Fact]
+        public void Test_InsertItem()
+        {
+            var data = new EfData();
+            var expected = new TodoItem() { ItemTitle = "Clean Room", ItemDescription = "Pick up clothes and vacuum.", Completed = false };
+
+            var actual = data.InsertItem(expected);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Test_RemoveItem()
+        {
+            var data = new EfData();
+            var idItemToRemove = 1;
+
+            var item = new TodoItem() { ItemTitle = "Clean Room", ItemDescription = "Pick up clothes and vacuum.", Completed = false };
+            data.InsertItem(item);
+
+            var actual = data.RemoveItem(item.Id);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Test_UpdateItem()
+        {
+            var data = new EfData();
+            var expected = new TodoItem() { ItemTitle = "Clean Room", ItemDescription = "Pick up clothes and vacuum.", Completed = false };
+
+            var actual = data.UpdateItem(expected, System.Data.Entity.EntityState.Added);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Test_UpdateCompletionStatus()
+        {
+            var data = new EfData();
+            bool completionStatus = true;
+
+            var item = new TodoItem() { ItemTitle = "Clean Room", ItemDescription = "Pick up clothes and vacuum.", Completed = false };
+            var updated = data.UpdateCompletionStatus(item, completionStatus);
+
+            Assert.True(item.Completed);
+        }
+
+        [Fact]
+        public void Test_GetTodoItems()
+        {
+            var data = new EfData();
+            var actual = data.GetTodoItems();
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void Test_GetTodoItemsComplete()
+        {
+            var data = new EfData();
+            var actual = data.GetTodoItemsComplete();
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void Test_GetTodoItemsIncomplete()
+        {
+            var data = new EfData();
+            var actual = data.GetTodoItemsIncomplete();
+            Assert.NotNull(actual);
+        }
+
+    }
+}
